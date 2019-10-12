6af83d52fe3d9412453e9199c0376891e718a609
lethalSid <senien.hicks@gmail.com>
2019-10-12 19:20:21 -0400

    added basic file structure

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+USE burgers_db;
+
+INSERT INTO burgers (name) VALUES ('Beef Burger');
+INSERT INTO burgers (name) VALUES ('Elk Burger');
+INSERT INTO burgers (name) VALUES ('Portobello Mushroom Burger');
+INSERT INTO burgers (name) VALUES ('Turkey Burger', true);
+INSERT INTO burgers (name) VALUES ('Impossible Burger', true);
+INSERT INTO burgers (name) VALUES ('Whiskey King Burger', true);
+INSERT INTO burgers (name) VALUES ('Luger Burger', true);

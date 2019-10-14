366f61bb0fe7fdeef3898b943644f3b49e9de802
lethalSid <senien.hicks@gmail.com>
2019-10-14 14:51:50 -0400

    fixed api-routes calls remember that routes must be 100 similar

--- public/assets/js/burger.js
+++ public/assets/js/burger.js
@@ -3,6 +3,7 @@ $(function() {
     $(".change-devoured").on("click", function(event) {
       var id = $(this).data("id");
       var newDevoured = $(this).data("newdevoured");
+      console.log(newDevoured);
 
       var newDevouredState = {
         devoured: newDevoured
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -14,7 +14,7 @@ module.exports = function(app) {
    app.get("/", function(req, res) {
     // findAll returns all entries for a table when used with no options
     db.Burger.findAll({}).then(function(dbBurger) {
-      console.log(dbBurger);
+      // console.log(dbBurger);
 
       // We have access to the Burgers as an argument inside of the callback function
       res.render("index", {burgers: dbBurger})
@@ -28,10 +28,12 @@ module.exports = function(app) {
     // create takes an argument of an object describing the item we want to insert
     // into our table. In this case we just we pass in an object with a name and
     // devoured property
-    db.Burger.create({
-      name: req.body.name,
-      devoured: req.body.devoured
-    }).then(function(dbBurger) {
+    db.Burger.create(
+      {
+        name: req.body.name,
+        devoured: req.body.devoured
+      }
+      ).then(function(dbBurger) {
       // We have access to the new Burger as an argument inside of the callback function
       res.json(dbBurger);
     });
@@ -54,15 +56,16 @@ module.exports = function(app) {
   });
 
   // PUT route for updating Burgers. We can get the updated Burger data from req.body
-  app.put("/api/burgers", function(req, res) {
+  app.put("/api/burgers/:id", function(req, res) {
+    console.log("BACKEND " +req.body);
+
     // Update takes in two arguments, an object describing the properties we want to update,
     // and another "where" object describing the burgers we want to update
     db.Burger.update({
-      name: req.body.name,
       devoured: req.body.devoured
     }, {
       where: {
-        id: req.body.id
+        id: req.params.id
       }
     })
       .then(function(dbBurger) {
